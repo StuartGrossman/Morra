3d016b29d0d5f8bab0eefaf9b65aa5b50836181c
Stuart Grossman <grossman.stuart1@gmail.com>
2025-04-01 19:48:33 -0700

    Update application with Firebase hosting and real-time multiplayer functionality

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,20 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      stream: path.resolve(__dirname, 'node_modules/stream-browserify'),
-      crypto: path.resolve(__dirname, 'node_modules/crypto-browserify'),
-      buffer: path.resolve(__dirname, 'node_modules/buffer'),
-      util: path.resolve(__dirname, 'node_modules/util'),
-      assert: path.resolve(__dirname, 'node_modules/assert'),
-      http: path.resolve(__dirname, 'node_modules/stream-http'),
-      https: path.resolve(__dirname, 'node_modules/https-browserify'),
-      os: path.resolve(__dirname, 'node_modules/os-browserify'),
-      url: path.resolve(__dirname, 'node_modules/url'),
+      '@': path.resolve(__dirname, './src'),
+      'stream': 'stream-browserify',
+      'http': 'stream-http',
+      'https': 'https-browserify',
+      'crypto': 'crypto-browserify',
+      'buffer': 'buffer',
+      'process': 'process/browser',
+      'util': 'util',
+      'assert': 'assert',
+      'url': 'url',
+      'zlib': 'browserify-zlib',
+      'path': 'path-browserify',
+      'fs': '',
+      'net': '',
+      'tls': '',
     },
   },
   define: {
@@ -23,7 +29,6 @@ export default defineConfig({
     global: 'globalThis',
   },
   optimizeDeps: {
-    include: ['buffer', 'crypto-browserify', 'stream-browserify'],
     esbuildOptions: {
       define: {
         global: 'globalThis',
